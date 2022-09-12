a27a1aaeb82e88b54e28b2dcef5c9b136559f021
Benoit de Chateauvieux <chateauv@amazon.com>
2022-09-12 14:58:56 -0400

    Populate Egrid DDB Table from JSON

--- test/grid_region_selector.test.ts
+++ test/grid_region_selector.test.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as GridRegionSelector from '../lib/grid_region_selector-stack';
+import * as GridRegionSelector from '../lib/grid-region-selector-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
